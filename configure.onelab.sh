2a95febc3bf6c2433351fd1e44afbe0ddee4888c
Loic Baron <loic.baron@gmail.com>
2018-09-07 12:04:36 +0000

    configure.onelab.sh use the onelab config into deploy/*.json

---
+++ configure.onelab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# This will configure the deployment
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH/deploy
+for f in *.json.example; do 
+    cp -- "$f" "${f%.json.example}.json"
+done
+for f in *.json.onelab; do 
+    cp -- "$f" "${f%.json.onelab}.json"
+done
+$SCRIPTPATH/test/creds/gen-certs.sh auth.noc.onelab.eu
